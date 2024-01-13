f9b27222f77b0888b1af95678d12f91cd8094bcb
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-13 10:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 //your JS code here. If required.
 fetch('https://jsonplaceholder.typicode.com/todos/1')
-	.then((response) => response.json())
-	.then((data) => console.log(data))
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.error('Fetch error:', error));
